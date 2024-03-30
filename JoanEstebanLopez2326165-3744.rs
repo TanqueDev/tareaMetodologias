ca52510b73690eb6fab04fa05bbe57578b974519
Joan Esteban López Narváez <joan.narvaez@correounivalle.edu.co>
2024-03-29 19:20:35 -0500

    Se corrige el archivo de código, agregando el nombre y código como una impresión por consola en el código de Rust

--- JoanEstebanLopez2326165-3744.rs
+++ JoanEstebanLopez2326165-3744.rs
@@ -1,6 +1,14 @@
 fn main() {
     println!("Hola, mundo!");
     println!("Adios, mundo!");
+
     let resultado = 2+2;
+
     println!("{}", resultado);
+
+    //
+    //
+    println!("Soy Joan Esteban López Narváez, Código 2326165!")
+    //
+    //
 }
